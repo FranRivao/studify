b07d8784abf3e4829950268cfe4f30ac8bfb3475
Rivao <francisco.rivao@alumnos.upm.es>
2025-04-11 19:17:13 +0200

    NoteView

---
+++ src/main/java/frangroup/studify/view/NoteView.java
@@ -0,0 +1,36 @@
+package frangroup.studify.view;
+
+import frangroup.studify.entity.Note;
+import frangroup.studify.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("api/v1/notes/")
+public class NoteView {
+    @Autowired
+    private final NoteService noteService;
+
+    public NoteView(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("create")
+    public String showCreate() {
+        return "index";
+    }
+
+    @PostMapping("create")
+    public String createProcess(@RequestParam String title, @RequestParam String description, Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+
+        this.noteService.saveOrUpdate(Note.builder().title(title).description(description).build());
+        return "resultado";
+    }
+}
