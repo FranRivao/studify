9d10f80272151db2dab24beacd342e9baf041abb
Rivao <francisco.rivao@alumnos.upm.es>
2025-04-11 19:15:45 +0200

    builder en entities

--- src/main/java/frangroup/studify/entity/Note.java
+++ src/main/java/frangroup/studify/entity/Note.java
@@ -2,9 +2,11 @@ package frangroup.studify.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
