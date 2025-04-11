2a1e536baa900a192ddb65efb144999a579a03e9
Rivao <francisco.rivao@alumnos.upm.es>
2025-04-11 17:32:40 +0200

    User entity

---
+++ src/main/java/frangroup/studify/entity/User.java
@@ -0,0 +1,23 @@
+package frangroup.studify.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "appUsers")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String password;
+}
