d41cafa4e4a83504731aaa475b83a5b824b45160
Rivao <francisco.rivao@alumnos.upm.es>
2025-04-12 17:36:59 +0200

    main route atr en System

--- src/main/java/frangroup/studify/StudifyApplication.java
+++ src/main/java/frangroup/studify/StudifyApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudifyApplication {
+	public final static String route = "api/v1/";
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudifyApplication.class, args);
 	}
-
 }
